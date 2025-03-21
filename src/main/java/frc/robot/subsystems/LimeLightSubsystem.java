90cdf2f5d7b6bd6518686bf4ef2645430142f632
Zachary Taylor <Zacharytaylor0508@gmail.com>
2025-03-20 21:26:19 -0500

    LL Skeleton

---
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimeLightSubsystem extends  SubsystemBase{
+public LimeLightSubsystem(){}
+public Command place(){
+    return null;
+}
+public void updateTracking(){}
+public Pose2d poseEst(){
+    return null;
+}
+@Override
+public void periodic() {
+    // TODO Auto-generated method stub
+    super.periodic();
+}
+}

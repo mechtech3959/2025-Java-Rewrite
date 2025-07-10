ba1ae41a50d0ba74a93681621d29ed25e872c315
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-07-10 14:40:43 -0500

    container cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,6 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
-import java.util.regex.MatchResult;
-
-import org.littletonrobotics.junction.LogTable;
 import org.littletonrobotics.junction.Logger;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
@@ -19,26 +16,17 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.wpilibj.XboxController;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.Claw.ClawSubsystem;
-import frc.robot.subsystems.Claw.ClawSubsystem.clawState;
 import frc.robot.commands.Intake;
 import frc.robot.commands.scoreL1;
 import frc.robot.commands.L1;
@@ -46,22 +34,14 @@ import frc.robot.commands.L2;
 import frc.robot.commands.L3;
 import frc.robot.commands.L4;
 import frc.robot.commands.Zero;
-import frc.robot.commands.travel;
 
-@SuppressWarnings("unused")
 
 public class RobotContainer {
-        private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
-                                                                                      // speed
-        private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per
-                                                                                          // second
-                                                                                          // max angular velocity
-
-        /* Setting up bindings for necessary control of the swerve drive platform */
+        private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed               
+        private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
         private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
                         .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-                        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive
-                                                                                 // motors
+                        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
         private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
         private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
         private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
@@ -71,14 +51,13 @@ public class RobotContainer {
 
         private final CommandXboxController joystick = new CommandXboxController(0);
         private final CommandXboxController coJoystick = new CommandXboxController(1);
-        private final XboxController cc = new XboxController(2);
+         
         public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
         public final ElevatorSubsystem elevator = new ElevatorSubsystem();
         public final ClawSubsystem claw = new ClawSubsystem();
         /* Path follower */
         private final SendableChooser<Command> autoChooser;
-        PrintCommand notA = new PrintCommand("NO");
-        PrintCommand A = new PrintCommand("NO");
+ 
         scoreL1 scorel1;
         L1 l1;
         L2 l2;
@@ -86,6 +65,9 @@ public class RobotContainer {
         L4 l4;
         Zero zero;
         Intake intake;
+        double finalH = 0;
+        double finalX = 0;
+        double clawAngle = 0;
 
         public RobotContainer() {
                 autoChooser = AutoBuilder.buildAutoChooser("Tests");
@@ -95,25 +77,13 @@ public class RobotContainer {
         }
 
         private void configureBindings() {
-                // Note that X is defined as forward according to WPILib convention,
-                // and Y is defined as to the left according to WPILib convention.
                 drivetrain.setDefaultCommand(
-                                // Drivetrain will execute this command periodically
-                                drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive
-                                                                                                                   // forward
-                                                                                                                   // with
-                                                                                                                   // negative
-                                                                                                                   // Y
-                                                                                                                   // (forward)
-                                                .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with
-                                                                                                // negative X (left)
-                                                .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive
-                                                                                                            // counterclockwise
-                                                                                                            // with
-                                                                                                            // negative
-                                                                                                            // X (left)
-                                ));
-
+                                drivetrain.applyRequest(() -> drive
+                                                .withVelocityX(-joystick.getLeftY() * MaxSpeed) // Negative Y(forward)
+                                                .withVelocityY(-joystick.getLeftX() * MaxSpeed)// Negative X(left)
+                                                .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // negative X(counterclockwise)
+                                                ));
+                                                                                                          
                 joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
                 joystick.b().whileTrue(drivetrain.applyRequest(
                                 () -> point.withModuleDirection(
@@ -142,74 +112,54 @@ public class RobotContainer {
 
                 drivetrain.registerTelemetry(logger::telemeterize);
                 // coJoystick.a().onChange(zero);
-                coJoystick.b().onChange(Commands.runOnce(() -> claw.setAxis(0.349)));//20
+                coJoystick.b().onChange(Commands.runOnce(() -> claw.setAxis(0.349)));// 20
                 coJoystick.back().onChange(Commands.runOnce(() -> claw.setAxis(0.0)));
 
-                coJoystick.x().onChange(Commands.runOnce(() -> claw.setAxis(0.698)));//40
-                coJoystick.a().onChange(Commands.runOnce(() -> claw.setAxis(2.61)));//150
+                coJoystick.x().onChange(Commands.runOnce(() -> claw.setAxis(0.698)));// 40
+                coJoystick.a().onChange(Commands.runOnce(() -> claw.setAxis(2.61)));// 150
                 coJoystick.y().onChange(Commands.runOnce(() -> elevator.setHeight(5)));
-
-                // coJoystick.y().onChange(l4);
-                // coJoystick.start().onChange(intake);
 
         }
 
-        double finalH = 0;
-        double finalX = 0;
-
         public void periodic() {
-                double p = claw.lastKnownAngle;
-                if (p == 0) {
+                clawAngle = claw.lastKnownAngle;
+                if (clawAngle == 0) {
                         finalH = 0;
                         finalX = 0;
-                } else if (p == 0.349) {
+                } else if (clawAngle == 0.349) {
                         finalH = 0.115;// 0.1415;
                         finalX = -0.11;// -0.13;
 
-                } else if (p == 0.698) {
+                } else if (clawAngle == 0.698) {
                         finalH = 0.259;
                         finalX = -0.1755;
 
-                } else if (p == 2.61) {
+                } else if (clawAngle == 2.61) {
                         finalH = 0.825;// 0.72;
                         finalX = 0.31;
 
                 }
-
-                Rotation3d c = new Rotation3d(drivetrain.getState().RawHeading);
-                Translation3d t = new Translation3d(drivetrain.getState().Pose.getTranslation());
-                Pose3d m = new Pose3d(t, c);
-                Pose3d g = new Pose3d(drivetrain.getState().Pose);
-
-                Translation3d q = new Translation3d(drivetrain.getState().Pose.getX(),
-                                drivetrain.getState().Pose.getY(), elevator.elevatorSim.getPositionMeters());
-                Translation3d carriage = new Translation3d(drivetrain.getState().Pose.getX(),
-                                drivetrain.getState().Pose.getY(), elevator.carriagElevatorSim.getPositionMeters());
-                // Pose3d q = new Pose3d(-x,-y,elevator.elevatorSim.getPositionMeters(),c);
-                // Pose3d carriage = new
-                // Pose3d(-x,-y,elevator.carriagElevatorSim.getPositionMeters(),c);
                 // using a sin reggression model translation should be
-                double modelY = 0.547343 * Math.sin((10.77753 * claw.sim.getAngleRads()) - 0.466346) + 0.821554;
-                double modelX = Math.asin(claw.sim.getAngleRads() - 0.821554 / 0.547343) + 0.466346 / 10.77753;
+                // double modelY = 0.547343 * Math.sin((10.77753 * claw.sim.getAngleRads()) -
+                // 0.466346) + 0.821554;
+                // double modelX = Math.asin(claw.sim.getAngleRads() - 0.821554 / 0.547343) +
+                // 0.466346 / 10.77753;
                 // this was wrong:sob:
-                Pose3d a = new Pose3d(q, c);
-                Pose3d b = new Pose3d(carriage, c);
-                Logger.recordOutput("myPose", g);
-                Logger.recordOutput("Myposearray", a, b);
-                Logger.recordOutput("Eout", q);
-                Logger.recordOutput("ele", elevator.visElevator);
+
+                Logger.recordOutput("myPose", new Pose3d(drivetrain.getState().Pose));
+                Logger.recordOutput("mech2d/elevator", elevator.elevatorMech);
                 Logger.recordOutput("final comp", new Pose3d[] {
-                                new Pose3d(0, 0, elevator.elevatorSim.getPositionMeters(), new Rotation3d(0, 0, 0)),
+                                new Pose3d(0, 0, elevator.elevatorSim.getPositionMeters(),
+                                                new Rotation3d(0, 0, 0)),
                                 new Pose3d(0, 0, elevator.carriagElevatorSim.getPositionMeters(),
                                                 new Rotation3d(0, 0, 0)),
                                 new Pose3d(finalX, 0, elevator.carriagElevatorSim.getPositionMeters() + finalH,
                                                 new Rotation3d(0, claw.sim.getAngleRads(), 0))
                 });
-
                 Logger.recordOutput("cal", new Pose3d[] {
                                 new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0)),
-                                new Pose3d(0, 0, 0,
-                                                new Rotation3d(0, 0, 0)),
+                                new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0)),
+                                new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0)),
                 });
 
         }

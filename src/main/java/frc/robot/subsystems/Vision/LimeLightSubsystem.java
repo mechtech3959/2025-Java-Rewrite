3fe6ed267abf9cb38bbecaae3fcaf43a36a8177d
Zachary Taylor <Zacharytaylor0508@gmail.com>
2025-08-11 08:38:42 -0500

    limelight init

--- src/main/java/frc/robot/subsystems/Vision/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/Vision/LimeLightSubsystem.java
@@ -1,11 +1,18 @@
 package frc.robot.subsystems.Vision;
 
+import frc.robot.subsystems.Vision.LimelightHelpers;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimeLightSubsystem extends SubsystemBase {
-    public LimeLightSubsystem() {
+   double TX;
+   double TY;
+   double TA;
+   String pipeLine;
+    public LimeLightSubsystem(String llName) {
+        pipeLine = llName;
     }
 
     public Command place() {
@@ -13,6 +20,10 @@ public class LimeLightSubsystem extends SubsystemBase {
     }
 
     public void updateTracking() {
+        TX = LimelightHelpers.getTX(pipeLine);
+        TY = LimelightHelpers.getTY(pipeLine);
+        TA = LimelightHelpers.getTA(pipeLine);
+    
     }
 
     public Pose2d poseEst() {

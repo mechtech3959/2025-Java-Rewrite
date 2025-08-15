aa8ae5b09cab7d4d0603bef3886687d51c108f31
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-08-15 14:40:53 -0500

    claw pose obj

---
+++ src/main/java/util/ClawPoseObject.java
@@ -0,0 +1,11 @@
+package util;
+
+public class ClawPoseObject {
+    private double angle =0;
+    public ClawPoseObject(Double angle){
+        this.angle = angle;
+    }
+    public double getClawAngle(){
+        return angle;
+    }
+}

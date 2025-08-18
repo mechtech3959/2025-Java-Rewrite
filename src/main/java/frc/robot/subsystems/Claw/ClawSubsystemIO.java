810a32e19354f72a447ec38cd7cd85b4df280505
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-08-18 08:53:56 -0500

    configurator

--- src/main/java/frc/robot/subsystems/Claw/ClawSubsystemIO.java
+++ src/main/java/frc/robot/subsystems/Claw/ClawSubsystemIO.java
@@ -1,10 +1,20 @@
 package frc.robot.subsystems.Claw;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
 public interface ClawSubsystemIO {
 
-    default void setAxis(double angle){}    
-    default void getAxis(){}    
-    default void resetAxis(){}
-    
+    default void setAxis(double angle) {
+    }
 
-} 
+    default void getAxis() {
+    }
+
+    default void resetAxis() {
+    }
+
+    default void configure(TalonFXConfiguration talonConfig,CANcoderConfiguration cancoderConfig) {
+    }
+
+}
--- src/main/java/frc/robot/subsystems/Claw/ClawSubsystemMotorIO.java
+++ src/main/java/frc/robot/subsystems/Claw/ClawSubsystemMotorIO.java
@@ -1,12 +1,24 @@
 package frc.robot.subsystems.Claw;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.measure.Velocity;
 
 public class ClawSubsystemMotorIO implements ClawSubsystemIO {
     private TalonFX axisMotor;
+    MotionMagicVoltage positionVoltage = new MotionMagicVoltage(0).withSlot(0);
+
     @Override
     public void setAxis(double angle) {
-        ClawSubsystemIO.super.setAxis(angle);
+        axisMotor.setControl(positionVoltage.withPosition(angle).withEnableFOC(true).withUseTimesync(true));
     }
-    
+    @Override
+    public void configure(TalonFXConfiguration talonConfig,CANcoderConfiguration cancoderConfig){
+        axisMotor.getConfigurator().apply(talonConfig);
+
+    }
 }

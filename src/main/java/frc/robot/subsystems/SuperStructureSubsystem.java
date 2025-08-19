d601e677cb9955f75612a2e18bda138c12c075d8
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-08-19 08:52:52 -0500

    setState

--- src/main/java/frc/robot/subsystems/SuperStructureSubsystem.java
+++ src/main/java/frc/robot/subsystems/SuperStructureSubsystem.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.commands.L2;
-import frc.robot.commands.L3;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.Claw.ClawSubsystem;
 import frc.robot.subsystems.Elevator.ElevatorSubsystem;
@@ -11,7 +9,8 @@ public class SuperStructureSubsystem extends SubsystemBase {
     private final ElevatorSubsystem elevator;
     private final ClawSubsystem claw;
     private final CommandSwerveDrivetrain drivetrain;
-    enum superState{
+
+    enum superState {
         Home,
         L1,
         L2,
@@ -19,18 +18,41 @@ public class SuperStructureSubsystem extends SubsystemBase {
         L4,
         Net
     }
-    public SuperStructureSubsystem(ElevatorSubsystem elevator, ClawSubsystem claw, CommandSwerveDrivetrain drivetrain)
-    {   this.elevator = elevator;
+
+    private final superState setSuperState;
+
+    public SuperStructureSubsystem(ElevatorSubsystem elevator, ClawSubsystem claw, CommandSwerveDrivetrain drivetrain,
+            superState setSuperState) {
+        this.elevator = elevator;
         this.claw = claw;
         this.drivetrain = drivetrain;
-        drivetrain = TunerConstants.createDrivetrain();    
-
-
+        this.setSuperState = setSuperState;
+        drivetrain = TunerConstants.createDrivetrain();
 
     }
+
+    void setState() {
+        switch (setSuperState) {
+            case Home:
+                break;
+            case L1:
+                break;
+            case L2:
+                break;
+            case L3:
+                break;
+            case L4:
+                break;
+            case Net:
+                break;
+            default:
+                break;
+        }
+    }
+
     @Override
-    public void periodic(){
+    public void periodic() {
 
     }
-    
+
 }

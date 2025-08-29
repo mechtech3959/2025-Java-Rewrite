523be2e0c7f64656c4df8214f0c4e0e79f363969
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-08-29 13:28:05 -0500

    add mapple sim

--- src/main/java/frc/robot/subsystems/Vision/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/Vision/LimeLightSubsystem.java
@@ -13,6 +13,7 @@ public class LimeLightSubsystem extends SubsystemBase {
    public boolean TV;
    public String pipeLine;
    public LimelightHelpers.PoseEstimate limelightMeasurement;
+
     public LimeLightSubsystem(String llName) {
         pipeLine = llName;
     }
@@ -20,13 +21,16 @@ public class LimeLightSubsystem extends SubsystemBase {
     public Command place() {
         return null;
     }
-
+    public void setRobot(double yaw){
+        LimelightHelpers.SetRobotOrientation(pipeLine,yaw,0,0,0,0,0);
+    
+    }
     public void updateTracking() {
         TX = LimelightHelpers.getTX(pipeLine);
         TY = LimelightHelpers.getTY(pipeLine);
         TA = LimelightHelpers.getTA(pipeLine);
         TV = LimelightHelpers.getTV(pipeLine);
-        limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue(pipeLine);
+        limelightMeasurement =  LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(pipeLine);
     
     }
 
